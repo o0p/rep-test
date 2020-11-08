75801510ef238ac314133bcc097af10fda7bc6a0
Russell <russelldev@protonmail.com>
2020-11-08 23:09:12 +0000

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const love = [
+    ['amanita crocea', 'amanita caesarea', 'amanita phalloides'],
+    ['sharks', 'dolphins', 'whales'],
+    ['stregth', 'fortitude', 'will']
+]
+
+for (let i = 0; i < love.length; i++) {
+    const lama = love[i];
+    for (let j = 0; j < lama.length; j++) {
+        console.log(lama[j]);
+    }
+}
