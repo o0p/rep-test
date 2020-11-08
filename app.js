b710c1db98010131ad8727663ef6f532735bb2e5
Russell <russelldev@protonmail.com>
2020-11-08 23:20:51 +0000

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let max = parseInt(prompt ('max num'));
 
 while(!max) {
-    let max = parseInt(prompt ('val num'));
+     max = parseInt(prompt ('val num'));
 }
 const sunNum = Math.floor(Math.random() * max) + 1;
 console.log(sunNum);
