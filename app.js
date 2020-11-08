9a3fff73bc29fe48268856aa3a333e5f4a68f275
Russell <russelldev@protonmail.com>
2020-11-08 23:29:16 +0000

    test

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const sunNum = Math.floor(Math.random() * max) + 1;
 console.log(sunNum);
 
 let g = parseInt(prompt('first g'));
+let tr = 1;
 while (g !== sunNum) {
+    tr++;
     if (g > sunNum) {
         g = parseInt(prompt('PT lvl'));
     } else {
         g = parseInt(prompt ('tl'));
     }
 }
-console.log ('scss')
+console.log (`'scss' ${tr}`)
