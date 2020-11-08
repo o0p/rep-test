b361f7c08d707e71dcac4275f046ac28d134e19b
Russell <russelldev@protonmail.com>
2020-11-08 23:25:06 +0000

    test

--- app.js
+++ app.js
@@ -7,5 +7,10 @@ console.log(sunNum);
 
 let g = parseInt(prompt('first g'));
 while (g !== sunNum) {
-
+    if (g > sunNum) {
+        g = prompt('PT lvl');
+    } else {
+        g = prompt ('tl');
+    }
 }
+consolel.log ('scss')
